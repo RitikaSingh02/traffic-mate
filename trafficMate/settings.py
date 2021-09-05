49607c5e5dc8768a062d0de3e3ab15ac639627a9
RitikaSingh02 <ritika2002singh@gmail.com>
2021-09-05 21:38:29 +0530

    deploying static file

--- trafficMate/settings.py
+++ trafficMate/settings.py
@@ -113,3 +113,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# print(BASE_DIR)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
