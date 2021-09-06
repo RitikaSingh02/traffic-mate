17b7634484ba4c424d3792dbcd1674d4b69b8919
RitikaSingh02 <ritika2002singh@gmail.com>
2021-09-06 19:07:43 +0530

    updates statics for weather

--- map/static/js/home.js
+++ map/static/js/home.js
@@ -2,7 +2,7 @@ function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+    alert("Geolocation is not supported by this browser.");
   }
 }
 
@@ -11,26 +11,66 @@ function showPosition(position) {
     var long = position.coords.longitude;
     console.log(lat , long)
     const data = { "lat": lat.toString()  , "long" : long.toString()};
+    localStorage.setItem("pick-lat" , lat);
+    localStorage.setItem("pick-long" , long);
+    var curr_origin = window.location.origin;
+    var url = curr_origin + '/maps/location';
     let response_data;
-    fetch('https://traffic-mate.herokuapp.com/maps/location',
+    fetch(url,
       {
         'method':'post',
         'body':JSON.stringify(data)
       }
-      ).then(function(res){
-        data=>res.json();
-      }).then(
-        (data) => {
-          response_data = data;
-        }
-      );
-        console.log(response_data);
+      ).then(
+        response=>response.json().then(
+          data=>(
+            {
+              data:data.data,
+              status:response.status
+            }
+          )
+        ).then(
+          res=>{
+            response_data = res.data;
+            console.log(response_data);             
+            document.getElementById("pick").value = response_data['address'];
+            // console.log(res.status , res.data);
+          }
+        )
+      )
+
 }
 function submit(){
-  const form = document.getElementsByClassName("form");
   const pick = $('#pick').val();
   const drop = $('#drop').val();
-  console.log(pick , drop);
-  const data = { "pick": pick  , "drop" : drop};
-  location.replace("https://traffic-mate.herokuapp.com/maps/main/" + pick +'/' + drop);
+  const data = {"drop" : drop};
+  let response_data;
+  var curr_origin = window.location.origin;
+  var url = curr_origin + '/maps/get_coordinates'
+  fetch(url,
+    {
+      'method':'post',
+      'body':JSON.stringify(data)
+    }
+    ).then(
+      response=>response.json().then(
+        data=>(
+          {
+            data:data.data,
+            status:response.status
+          }
+        )
+      ).then(
+        res=>{
+          response_data = res.data;
+          console.log(response_data);       
+          localStorage.setItem("drop-lat" , response_data['lat']);
+          localStorage.setItem("drop-long" , response_data['long']);
+          console.log(pick , drop);
+          // const data = { "pick": pick  , "drop" : drop};
+          window.location.href = curr_origin + "/maps/main/" + pick +'/' + drop;      
+        }
+      )
+    )
+
 }
